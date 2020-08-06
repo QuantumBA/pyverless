51738159516335ec69cd872443675ecff4f5c017
Xiang Chen <xchen@foqum.io>
2020-08-06 16:32:27 +0200

    Bump min PyYAML version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.18',
+    version='0.0.19',
 
     description='A mini-framework providing tools to help you make complex APIs with serverless',
     long_description=long_description,
@@ -77,7 +77,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'PyJWT>=1.5.2',
-        'PyYAML>=3.12',
+        'PyYAML>=5.1',
         'sentry-sdk>=0.5.1'
     ],
 
