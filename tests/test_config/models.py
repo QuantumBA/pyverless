3df81783e9460956aa70067b549c7b6373f27014
Xiang Chen <xchen@foqum.io>
2020-12-10 12:11:34 +0100

    Add github workflow

--- tests/test_config/models.py
+++ tests/test_config/models.py
@@ -4,9 +4,25 @@ from pyverless.serializers import Serializer
 hasher = PBKDF2PasswordHasher()
 
 
+class objectsMeta(type):
+    def __len__(self):
+        return 2
+
+    def __getitem__(cls, position):
+        if isinstance(position, slice):
+            return cls.all()
+        else:
+            if position >= len(cls):
+                raise IndexError
+            if position >= 0:
+                return cls.all()[position]
+            else:
+                return cls.all()[position]
+
+
 class User:
 
-    class objects:
+    class objects(metaclass=objectsMeta):
 
         def get_or_none(**kwargs):
             return User(email='one@users.com', password='test-password')
