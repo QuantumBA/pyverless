d8c7c6938267178d098b9222be0d68821e301e86
Sergio <smartinez@foqum.io>
2024-07-03 11:13:28 +0200

    :rocket: Added standalone for streaming response

--- pyverless/api_gateway_handler/api_gateway_handler_standalone.py
+++ pyverless/api_gateway_handler/api_gateway_handler_standalone.py
@@ -36,3 +36,20 @@ class ApiGatewayWSHandlerStandalone(ApiGatewayWSHandler, ABC):
             "statusCode": self.response.status_code,
             "body": json.dumps(self.response.body),
         }
+
+class ApiGatewayStreamingHandlerStandalone(ApiGatewayHandler, ABC):
+    headers: Dict = {}
+
+    def render_response(self):
+        headers = {
+            "Access-Control-Allow-Origin": settings.CORS_ORIGIN,
+            "Access-Control-Allow-Headers": settings.CORS_HEADERS,
+            "Access-Control-Allow-Methods": "*",
+        }
+        if self.headers:
+            headers = {**headers, **self.headers}
+        return {
+            "statusCode": self.response.status_code,
+            "body": self.response.body,
+            "headers": headers,
+        }
